919f267184bffec8ca2a9aca6f9aaf73f51b15ad
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2022-01-19 09:11:49 +0900

    Update 10_type_num.go

--- go/getting_started/3_definition/10_type_num.go
+++ go/getting_started/3_definition/10_type_num.go
@@ -15,15 +15,17 @@ func main() {
 
 	x := 1 + 1
 	fmt.Println(x)
-	fmt.Println(1+1, 2+2)
-	fmt.Println("1 + 1 =", 1+1)
-	fmt.Println("10 - 1 =", 10-1)
-	fmt.Println("10 / 2 =", 10/2)
-	fmt.Println("10 / 3 =", 10/3)
-	fmt.Println("10.0 / 3 =", 10.0/3)
-	fmt.Println("10 / 3.0 =", 10/3.0)
-	fmt.Println("10 % 2 =", 10%2)
-	fmt.Println("10 % 3 =", 10%3)
+	fmt.Println(1 + 1, 2 + 2)
+	fmt.Println(" 1 + 1 =", 1 + 1)
+	fmt.Println("10 - 1 =", 10 - 1)
+	
+	fmt.Println("10   / 2   =", 10 / 2)
+	fmt.Println("10   / 3   =", 10 / 3)
+	fmt.Println("10.0 / 3   =", 10.0 / 3)
+	fmt.Println("10   / 3.0 =", 10 / 3.0)
+	
+	fmt.Println("10 % 2 =", 10 % 2)
+	fmt.Println("10 % 3 =", 10 % 3)
 
 	x = 0
 	fmt.Println(x)
